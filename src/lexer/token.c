a2d9c946018ee4143f3dbca72f3447503537be88
mariosrb <mario-2000@hotmail.fr>
2025-04-13 15:50:16 +0200

    token

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -1,5 +1,39 @@
 #include "../../include/minishell.h"
 
+static char	*extract_word(t_lexer *lexer)
+{
+	char	*word;
+	int		start;
+	int		len;
+	char	quote;
+
+	if (!lexer || lexer->pos >= lexer->length)
+		return (NULL);
+	start = lexer->pos;
+	len = 0;
+	quote = 0;
+	while (lexer->pos < lexer->length)
+	{
+		if (!quote && (lexer->current == ' ' || is_delimiter_start(lexer->current)))
+			break;
+		if (is_quote(lexer->current))
+		{
+			if (quote == 0)
+				quote = lexer->current;
+			else if (quote == lexer->current)
+				quote = 0;
+		}
+		len++;
+		advance(lexer);
+	}
+	word = malloc (sizeof(char) * (len + 1));
+	if (!word)
+		return (NULL);
+	ft_strlcpy(word, &lexer->input[start], len + 1);
+	word[len] = '\0';
+	return (word);
+}
+
 t_token *tokenize(char *input)
 {
 	t_lexer *lexer;
@@ -9,7 +43,7 @@ t_token *tokenize(char *input)
 
 	head = NULL;
 	processed_input = preprocess_input(input);
-	if (!preprocess_input)
+	if (!processed_input)
 		return (NULL);
 	lexer = init_lexer(processed_input);
 	if (!lexer)
@@ -20,13 +54,13 @@ t_token *tokenize(char *input)
 	while (lexer->pos < lexer->length)
 	{
 		skip_whitespace(lexer);
-		if (lexer->pos < lexer->length)
+		if (lexer->pos >= lexer->length)
 			break;
 		// Verifier si on est sur un delimiteur
 		if (is_delimiter_start(lexer->current))
 		{
 			t_token_type type = get_operator_type(lexer);
-			char *value = extract_operator_value(type);
+			char *value = extract_operator_value(lexer);
 			new_token = create_token(type, value, 0);
 		}
 		// sinon cest un mot ou un arg
