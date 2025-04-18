fe91f788b153b329b3982f0a1385a7d2c992d107
mariosrb <mario-2000@hotmail.fr>
2025-04-18 16:46:12 +0200

    tokenize --> extract word

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -4,19 +4,22 @@ static char	*extract_word_with_quotes(t_lexer *lexer)
 {
 	char	*word;
 	int		i;
-	int		j;
+	int		len;
 	char	c;
 
 	i = 0;
-	j = 0;
-	word = malloc (sizeof(char) * extract_word_length(lexer) + 1);
+	len = extract_word_length(lexer);
+	word = malloc (sizeof(char) * len + 1);
 	if (!word)
 		return (NULL);
-	while (lexer->pos < lexer->length)
+	while (lexer->pos < lexer->length && i < len)
 	{
-		c = (lexer->input[lexer->pos + i]);
-		
+		c = (lexer->input[lexer->pos]);
+		word[i] = c;
+		advance(lexer);
+		i++;
 	}
+	word[i] = '\0';
 	return (word);
 }
 
@@ -36,23 +39,22 @@ t_token *tokenize(char *input)
 
 	// "ls -la" | wc -l >> outfile "'"$USER"'"
 	printf("\nPrepro : %s\n", processed_input);
-	//head = NULL;
-	char *value = extract_word_with_quotes(lexer);
-	// while (lexer->pos < lexer->length)
-	// {
-	// 	skip_whitespace(lexer);
-	// 	if (is_delimiter_start(lexer->current))
-	// 	{
-	// 		t_token_type type = get_operator_type(lexer);
-	// 		char *value = extract_operator_value(lexer);
-	// 		new_token = create_token(type, value, 0);
-	// 	}
-	// 	else
-	// 	{
-	// 		char *value = extract_word_with_quotes(lexer);
-	// 	}
-	// 	add_token_to_list(head, new_token);
-	// 	advance(lexer);
-	// }
+	head = NULL;
+	while (lexer->pos < lexer->length)
+	{
+		skip_whitespace(lexer);
+		if (is_delimiter_start(lexer->current))
+		{
+			t_token_type type = get_operator_type(lexer);
+			char *value = extract_operator_value(lexer);
+			new_token = create_token(type, value, 0);
+			printf("\n%s\n", new_token->value);
+		}
+		else
+		{
+			char *value = extract_word_with_quotes(lexer);
+		}
+		add_token_to_list(&head, new_token);
+	}
 	return (head);
 }
--- src/lexer/token_utils.c
+++ src/lexer/token_utils.c
@@ -26,7 +26,7 @@ int extract_word_length(t_lexer *lexer)
 	while (lexer->pos + i < lexer->length)
 	{
 		c = lexer->input[lexer->pos + i];
-		printf("%c | %c\n",c, lexer->input[lexer->pos + 1 + i]);
+		//printf("%c | %c\n",c, lexer->input[lexer->pos + 1 + i]);
 		if (is_quote(c))
 		{
 			if (quote == 0)
@@ -40,7 +40,7 @@ int extract_word_length(t_lexer *lexer)
 			break;
 		i++;
 	}
-	printf("%d\n", i);
+	//printf("%d\n", i);
 	return (i);
 }
 
