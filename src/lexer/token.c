18c739031020dec2bb77df46d8a8c8c2bb3818ee
mariosrb <mario-2000@hotmail.fr>
2025-05-15 15:08:06 +0200

    dernier test funcheck passe

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -36,7 +36,54 @@ int init_lexer_preprocess(t_lexer **lexer, char **p_input, char *input)
 	return (1);
 }
 
+static t_token	*operator_step(t_lexer *lexer, t_token *head, char *p_input)
+{
+	t_token_type	type;
+	t_token			*new_token;
 
+	type = get_operator_type(lexer);
+	if (!extract_operator_value(lexer))
+	{
+		free_tok_error(lexer, head, NULL, p_input);
+		return (NULL);
+	}
+	new_token = create_operator_token(type);
+	if (!new_token)
+	{
+		free_tok_error(lexer, head, NULL, p_input);
+		return (NULL);
+	}
+	return (new_token);
+}
+
+static t_token	*word_step(t_lexer *lexer, t_token *head, char *p_input)
+{
+	char	*value;
+	t_word	*word;
+	t_token	*new_token;
+
+	value = extract_word_with_quotes(lexer);
+	if (!value)
+	{
+		free_tok_error(lexer, head, NULL, p_input);
+		return (NULL);
+	}
+	word = NULL;
+	clean_words(value, &word);
+	if (!word)
+	{
+		free_tok_error(lexer, head, value, p_input);
+		return (NULL);
+	}
+	free(value);
+	new_token = create_word_token(TOKEN_WORD, word);
+	if (!new_token)
+	{
+		free_tok_error(lexer, head, value, p_input);
+		return (NULL);
+	}
+	return (new_token);
+}
 
 // Transforme l’entrée utilisateur en tokens distincts en traitant opérateurs, mots, espaces et quotes
 t_token *tokenize(char *input)
@@ -46,53 +93,21 @@ t_token *tokenize(char *input)
 	t_token *new_token;
 	char *processed_input;
 
+	head = NULL;
 	if (!init_lexer_preprocess(&lexer, &processed_input, input))
 	{
-		free_tok_error(lexer, head, NULL, processed_input);
-		return (NULL);
+		processed_input = NULL;
+		return (free_tok_error(lexer, head, NULL, processed_input), NULL);
 	}
-	head = NULL;
 	while (lexer->pos < lexer->length)
 	{
 		skip_whitespace(lexer);
 		if (is_delimiter_start(lexer->current))
-		{
-			t_token_type type = get_operator_type(lexer);
-			if (!extract_operator_value(lexer))
-			{
-				free_tok_error(lexer, head, NULL, processed_input);
-				return (NULL);
-			}
-			new_token = create_operator_token(type);
-			if (!new_token)
-			{
-				free_tok_error(lexer, head, NULL, processed_input);
-				return (NULL);
-			}
-		}
+			new_token = operator_step(lexer, head, processed_input);
 		else
-		{
-			char *value = extract_word_with_quotes(lexer);
-			if (!value)
-			{
-				free_tok_error(lexer, head, NULL, processed_input);
-				return (NULL);
-			}
-			t_word *word = NULL;
-			clean_words(value, &word);
-			if (!word)
-			{
-				free_tok_error(lexer, head, value, processed_input);
-				return (NULL);
-			}
-			free(value);
-			new_token = create_word_token(TOKEN_WORD, word);
-			if (!new_token)
-			{
-				free_tok_error(lexer, head, value, processed_input);
-				return (NULL);
-			}
-		}
+			new_token = word_step(lexer, head, processed_input);
+		if (!new_token)
+			return (NULL);
 		add_token_to_list(&head, new_token);
 	}
 	free(lexer);
--- src/main.c
+++ src/main.c
@@ -12,7 +12,8 @@ int	main(int ac, char **av, char **envp)
 	handle_signal();
 	while (1)
 	{
-		input = readline("minishell> ");
+		input = "ls -l |wc-l>>outfile $USER'PATH'";
+		//input = readline("minishell> ");
 		if (!input)
 		{
 			write (1, "exit\n", 5);
@@ -21,13 +22,13 @@ int	main(int ac, char **av, char **envp)
 		// si l'input n'est pas vide on le traite
 		if (*input)
 		{
-			add_history(input);
+			//add_history(input);
 			printf("\n%s\n", input);
 			t_token *head = NULL;
 			head = tokenize(input);
 			if (!head)
 			{
-				free(input);
+				//free(input);
 				return (1);
 			}
 			printf("\n");
